8e54108536cff22a9acc71c8ce989f27b2f3c024
messat <148489126+messat@users.noreply.github.com>
2024-06-18 12:24:37 +0100

    Challenge 06: Added comments below each article on selection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,7 @@ function Navbar (){
  return (
     <nav className="NavBar">
         <Link to="/"><Button className="homeButton" variant="contained">Home</Button></Link>        
-        <Button variant="contained">Comments on Article</Button>
-        <Button variant="contained">Vote Article</Button>
         <Button variant="contained">Post New Comment</Button>
-        <Button variant="contained">Remove Comment</Button>
         <Button variant="contained">Topic Of Articles</Button>
         <Button variant="contained">Sort Article Search</Button>
     </nav>
